a95c260e029ed1e928b9483f3ba8e26ab9cbda88
daygot <46773816+daygot@users.noreply.github.com>
2020-06-19 15:50:55 -0700

    Splash screen 1.1

--- app/src/main/java/com/goats/briller/SplashScreen.java
+++ app/src/main/java/com/goats/briller/SplashScreen.java
@@ -11,7 +11,7 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        Intent intent = new Intent(this, SignInScreen.class);
+        Intent intent = new Intent(this, LandingScreen.class);
         startActivity(intent);
         finish();
     }
