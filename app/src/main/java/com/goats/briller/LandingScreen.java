ae768e5eaf5513d1612745ff7b05f41f7521791f
daygot <46773816+daygot@users.noreply.github.com>
2020-07-16 19:53:39 -0700

    Initializing

--- app/src/main/java/com/goats/briller/LandingScreen.java
+++ app/src/main/java/com/goats/briller/LandingScreen.java
@@ -1,7 +1,6 @@
 package com.goats.briller;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,7 +27,7 @@ public class LandingScreen extends AppCompatActivity {
     }
 
     public void OpenSignInScreen() {
-        Intent signInIntent = new Intent(this, SignInScreen.class);
+        Intent signInIntent = new Intent(this, OnboardingScreen.class);
         startActivity(signInIntent);
     }
 
---
+++ app/src/main/java/com/goats/briller/OnboardingScreen.java
@@ -0,0 +1,13 @@
+package com.goats.briller;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class OnboardingScreen extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding_screen);
+    }
+}
